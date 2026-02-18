0cd0bc8f91e29905da8291429857948bc8612077
Genie-hacqman <ii813119@gmail.com>
2026-02-18 09:10:47 +0000

    Hackman:Assignment

--- Hackman/assignment/assign.py
+++ Hackman/assignment/assign.py
@@ -1,4 +1,13 @@
 """
 create a custom function that will check for parity.
-parity 
+to determine if a number is even or odd.
 """
+def parity():
+    number = 2
+    number = int(input("Enter a number: "))
+    if number % 2 == 0:
+        print(f"{number} is even.")
+    else:
+        print(f"{number} is odd.")
+
+parity()        
